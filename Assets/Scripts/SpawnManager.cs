0e650600fe611274285fdfb0664c8f5ba26b31f2
TBFgunmo <gunmohome123@gmail.com>
2026-03-28 23:56:20 +0700

    first

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -5,9 +6,72 @@ public class SpawnManager : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject enemyPrefab;
 
+    private Coroutine byeRoutune;
+
     void Start()
     {
+        //InvokeRepeating(nameof(RandomSpawn), 0, 3);
+        StartCoroutine(SpawnRoutine());
+        //byeRoutune = StartCoroutine(Bye());
+    }
 
+    private void Update()
+    {
+        //if (Time.time > 3) 
+        //{
+        //    StopCoroutine(byeRoutune);   //stop ���������������� null
+        //    //StopAllCoroutines();
+        //}
+    }
+
+    void RandomSpawn()
+    {
+        var index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[index];
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+
+    IEnumerator Hello(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+    }
+
+    IEnumerator Bye()
+    {
+        while (true)
+        {
+            Debug.Log("Bye" + Time.frameCount + " " + Time.time);
+            //yield return null;
+            yield return new WaitForSeconds(1f);
+            yield return Hello(4);
+            //StartCoroutine(Hello());
+            yield return new WaitForSeconds(1f);
+            //if (Time.time > 5) 
+            //{
+            //    yield break;
+            //}
+        }
     }
 
 }
